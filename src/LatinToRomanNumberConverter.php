8fc20fcc27d11c63f05c99633e80ca539dd61d09
Rezouce <contact@rezouce.net>
2017-03-10 17:28:51 +0100

    Convert 5, 10, 50, 100, 500 and 1000 respectively to V, X, L, C, D and M.

--- src/LatinToRomanNumberConverter.php
+++ src/LatinToRomanNumberConverter.php
@@ -6,6 +6,15 @@ class LatinToRomanNumberConverter
 
     public function convert(int $latinNumber)
     {
-        return str_pad('', $latinNumber, 'I');
+        $romanNumber = str_pad('', $latinNumber, 'I');
+
+        $romanNumber = str_replace('IIIII', 'V', $romanNumber);
+        $romanNumber = str_replace('VV', 'X', $romanNumber);
+        $romanNumber = str_replace('XXXXX', 'L', $romanNumber);
+        $romanNumber = str_replace('LL', 'C', $romanNumber);
+        $romanNumber = str_replace('CCCCC', 'D', $romanNumber);
+        $romanNumber = str_replace('DD', 'M', $romanNumber);
+
+        return $romanNumber;
     }
 }
--- tests/LatinToRomanNumberConverterTest.php
+++ tests/LatinToRomanNumberConverterTest.php
@@ -22,4 +22,52 @@ class LatinToRomanNumberConverterTest extends TestCase
 
         $this->assertEquals('II', $subject->convert(2));
     }
+
+    /** @test */
+    public function can_convert_5_to_V()
+    {
+        $subject = new LatinToRomanNumberConverter();
+
+        $this->assertEquals('V', $subject->convert(5));
+    }
+
+    /** @test */
+    public function can_convert_10_to_X()
+    {
+        $subject = new LatinToRomanNumberConverter();
+
+        $this->assertEquals('X', $subject->convert(10));
+    }
+
+    /** @test */
+    public function can_convert_50_to_L()
+    {
+        $subject = new LatinToRomanNumberConverter();
+
+        $this->assertEquals('L', $subject->convert(50));
+    }
+
+    /** @test */
+    public function can_convert_100_to_C()
+    {
+        $subject = new LatinToRomanNumberConverter();
+
+        $this->assertEquals('C', $subject->convert(100));
+    }
+
+    /** @test */
+    public function can_convert_500_to_D()
+    {
+        $subject = new LatinToRomanNumberConverter();
+
+        $this->assertEquals('D', $subject->convert(500));
+    }
+
+    /** @test */
+    public function can_convert_1000_to_M()
+    {
+        $subject = new LatinToRomanNumberConverter();
+
+        $this->assertEquals('M', $subject->convert(1000));
+    }
 }
