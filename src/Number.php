5b96dd0e5a85d8deae0c9d5f5e6b2a4794ef4177
Rezouce <contact@rezouce.net>
2017-04-02 16:17:44 +0200

    Check all numbers from 3 to 7919 (which contains the 1000 first prime numbers) to see if they're prime.

--- src/Number.php
+++ src/Number.php
@@ -12,13 +12,30 @@ class Number
         $this->int = $int;
     }
 
-    /**
-     * The only prime pair number is 2.
-     *
-     * @return bool
-     */
     public function isPrime()
     {
-        return $this->int == 2;
+        // 0 and 1 are not prime numbers.
+        if ($this->int < 2) {
+            return false;
+        }
+
+        // 2 is a prime number.
+        if ($this->int == 2) {
+            return true;
+        }
+
+        // All other pair numbers are not prime.
+        if ($this->int % 2 == 0) {
+            return false;
+        }
+
+        // If a number can be divided by anything else than 1 or itself, it's not a prime number.
+        for ($i = 3; $i < $this->int; $i += 2) {
+            if ($this->int % $i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- tests/NumberTest.php
+++ tests/NumberTest.php
@@ -40,4 +40,20 @@ class NumberTest extends TestCase
             $this->assertFalse($number->isPrime());
         }
     }
+
+    /** @test */
+    public function check_if_numbers_from_3_to_1000_are_primes()
+    {
+        $listOfPrimeNumbers = require __DIR__ . '/../resources/1000_first_prime_numbers.php';
+
+        for ($i = 0; $i <= end($listOfPrimeNumbers); ++$i) {
+            $number = new Number($i);
+
+            if (in_array($i, $listOfPrimeNumbers)) {
+                $this->assertTrue($number->isPrime(), sprintf('The number %s should be prime', $i));
+            } else {
+                $this->assertFalse($number->isPrime(), sprintf('The number %s should not be prime', $i));
+            }
+        }
+    }
 }
