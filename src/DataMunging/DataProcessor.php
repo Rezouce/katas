6c709194efd3ecd50c6f7c9d9ab270970c7a09ad
Sebastien Breysse <sebastien.breysse@doclerholding.com>
2017-06-16 08:59:17 +0200

    Extract data processing from Football and Weather classes for part 3 of the data munging.

---
+++ src/DataMunging/DataProcessor.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Kata\DataMunging;
+
+class DataProcessor
+{
+
+	public function process(string $data): array
+	{
+		$data = $this->explodeLines($data);
+
+		$data = $this->removeLines(2, $data);
+		$data = $this->removeEmptyLines($data);
+
+		return $this->extractData($data);
+	}
+
+	private function explodeLines(string $data): array
+	{
+		return explode("\n", $data);
+	}
+
+	private function removeLines(int $numberLines, array $data): array
+	{
+		return array_slice($data, $numberLines);
+	}
+
+	private function removeEmptyLines(array $data): array
+	{
+		return array_filter($data, function ($line) {
+			return !empty($line);
+		});
+	}
+
+	private function extractData(array $data): array
+	{
+		return array_map(function ($line) {
+			return array_values(array_filter(explode(' ', $line), function ($data) {
+				return !empty($data);
+			}));
+		}, $data);
+	}
+}
--- src/DataMunging/Football.php
+++ src/DataMunging/Football.php
@@ -9,43 +9,14 @@ class Football
 
 	public function __construct($data)
 	{
-		$this->data = $this->processData($data);
+		$data = (new DataProcessor())->process($data);
+
+		$this->data = $this->mapData($data);
 	}
 
-	private function processData(string $data)
+	private function mapData(array $data): array
 	{
-		$data = $this->explodeLines($data);
-
-		$data = $this->removeLines(2, $data);
-		$data = $this->removeEmptyLines($data);
-
-		return $this->extractData($data);
-	}
-
-	private function explodeLines(string $data): array
-	{
-		return explode("\n", $data);
-	}
-
-	private function removeLines(int $numberLines, array $data): array
-	{
-		return array_slice($data, $numberLines);
-	}
-
-	private function removeEmptyLines(array $data): array
-	{
-		return array_filter($data, function ($line) {
-			return !empty($line);
-		});
-	}
-
-	private function extractData(array $data): array
-	{
-		return array_map(function ($line) {
-			$data = array_values(array_filter(explode(' ', $line), function ($data) {
-				return !empty($data);
-			}));
-
+		return array_map(function ($data) {
 			return [
 				'team' => $data[1],
 				'difference' => abs($data[8] - $data[6]),
